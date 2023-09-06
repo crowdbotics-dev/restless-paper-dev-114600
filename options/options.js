c2f5c0f67eb585336f501ab6ca80f9be22b4ec1e
Crowdbotics <engineering@crowdbotics.com>
2023-09-06 23:18:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-paper-dev-114600",
+  url: "https://restless-paper-dev-114600.botics.co",
+  api: "https://restless-paper-dev-114600.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
